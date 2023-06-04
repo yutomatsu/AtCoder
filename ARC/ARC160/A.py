fab90873772900fbe383a205313fed0e5579d024
yutomatsu <102792097+yutomatsu@users.noreply.github.com>
2023-06-04 17:29:28 +0900

    Update A.py

--- ARC/ARC160/A.py
+++ ARC/ARC160/A.py
@@ -1,1 +1,31 @@
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
 
+idx = [-1]*N
+for i in range(N):
+    idx[A[i]-1] = i
+
+B = []
+seen = set()
+for i in range(N):
+    num = 1
+    while K:
+        if num in seen:
+            num += 1
+            continue
+        if num==A[i]:
+            cnt = N+(N-i-1)*(N-i-2)//2
+        else:
+            cnt = 1
+        if K>cnt:
+            K -= cnt
+        else:
+            break
+        num += 1
+    if num!=A[i]:
+        B = B+A[i:idx[num-1]+1][::-1]+A[idx[num-1]+1:]
+        break
+    else:
+        B.append(num)
+        seen.add(num)
+print(*B)
